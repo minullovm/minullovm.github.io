a87a39c55b1b4a03a76aa589cb0543a0a010ec07
minullovm <96243331+minullovm@users.noreply.github.com>
2022-01-12 12:35:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ let tryCount = 1;
 let maxTryCount = 7;
 for (tryCount; tryCount < 7; tryCount++) {
     if (number == numberRandom) {
-       alert('Верно!<br>');
+       alert('Верно!');
         break;
     } else if (number > numberRandom) {
        alert('Загаданное число меньше вашего');
     } else {
        alert('Загаданное число больше вашего');
     }
-    number = +prompt('Попробуйте ещё раз, Угадайте число от 1 до 100\nУ вас осталось ' + (maxTryCount - tryCount) + ' попыток');
+    number = +prompt('Попробуйте ещё раз\nУгадайте число от 1 до 100\nУ вас осталось ' + (maxTryCount - tryCount) + ' попыток');
+}
+
+if(tryCount == maxTryCount) {
+    alert ('Вы проиграли! Попробуйте заново')
 }
