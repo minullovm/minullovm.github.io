67882b102f218fdf295cbd5abbb18466e6b59549
minullovm <96243331+minullovm@users.noreply.github.com>
2022-01-12 12:27:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 let numberRandom = Math.round(Math.random() * 100);
 console.log(numberRandom);
-let number = prompt('Введите число от 1 до 100\nУ вас есть 7 попыток');
+let number = prompt('Угадайте число от 1 до 100\nУ вас есть 7 попыток');
 
 let tryCount = 1;
 let maxTryCount = 7;
@@ -15,5 +15,5 @@ for (tryCount; tryCount < 7; tryCount++) {
     } else {
        alert('Загаданное число больше вашего');
     }
-    number = +prompt('Попробуйте ещё раз, Введите число от 1 до 100\nУ вас осталось ' + (maxTryCount - tryCount) + ' попыток');
+    number = +prompt('Попробуйте ещё раз, Угадайте число от 1 до 100\nУ вас осталось ' + (maxTryCount - tryCount) + ' попыток');
 }
