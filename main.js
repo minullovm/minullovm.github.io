9588393c794ff0f9d4cef8d3a9291dffdb639d72
minullovm <96243331+minullovm@users.noreply.github.com>
2022-01-21 15:12:29 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,48 @@
+const divv = document.querySelector('div')
+const div = document.createElement('div')
+
+
+setTimeout(function () {
+    div.innerText = 'Даша'
+    divv.append(div)
+}, 1000);
+
+setTimeout(function () {
+    div.innerText = 'Даша. Ну что?'
+    divv.append(div)
+}, 2000);
+
+setTimeout(function () {
+    div.innerText = 'Даша. Ну что? Как дела?'
+    divv.append(div)
+}, 3000);
+
+setTimeout(function () {
+    div.innerText = 'Даша. Ну что? Как дела? Сучка'
+    divv.append(div)
+}, 4000);
+
+setTimeout(function () {
+    div.innerText = 'Даша. Ну что? Как дела? Сучка :)'
+    divv.append(div)
+}, 5000);
+
+setTimeout(function () {
+    div.innerText = 'Даша. Ну что? Как дела? Сучка :) Смотри'
+    divv.append(div)
+}, 6000);
+
+setTimeout(function () {
+    div.innerText = 'Даша. Ну что? Как дела? Сучка :) Смотри что'
+    divv.append(div)
+}, 7000);
+
+setTimeout(function () {
+    div.innerText = 'Даша. Ну что? Как дела? Сучка :) Смотри что умею'
+    divv.append(div)
+}, 8000);
+
+setTimeout(function () {
+    div.innerText = 'Даша. Ну что? Как дела? Сучка :) Смотри что умею :)'
+    divv.append(div)
+}, 8000);
